a229a322e60516a1761c1d401aa57b2102466812
yvanyang <mail4yangzq@gmail.com>
2022-08-29 11:01:44 +0800

    rtf

--- vscode_cmake_demo/main.cpp
+++ vscode_cmake_demo/main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 
-using namespace std;
+// using namespace std;
 
+// int main() {
+//     int value = 222333;
+//     cout << value << endl;
+//     return 0;
+// }
+
+
+std::string rtfToHtml(std::string rtf) {
+    return rtf;
+}
 int main() {
-    int value = 1;
-    cout << value << endl;
+    std::string res = rtfToHtml("hjkl");
+    std::cout << res << std::endl;
     return 0;
 }
